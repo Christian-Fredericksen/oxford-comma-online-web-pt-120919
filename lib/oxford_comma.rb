0e21a2da95e8ace4eb3590b5567a171e0f84ec4c
Christian Fredericksen <christian.fredericksen@yahoo.com>
2019-12-18 05:16:32 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array)
   #array.join(" and ")
   if array.length > 2
-    array[2] = "and #{array[2]}"
+    array[last] = "and #{array[last]}"
     array.join(", ")
     
      else
