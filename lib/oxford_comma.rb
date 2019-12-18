2fc3db85858c5e922c6e4c0d07d11a7a24d81457
Christian Fredericksen <christian.fredericksen@yahoo.com>
2019-12-18 04:30:04 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  #array.join(" and ")
+  if array.length > 2
+    array.last {|word| "and #{word}"}
+    array.join(", ")
+    
+     else
+    array.join(" and ")
+    
+  end
+ 
+ 
 end
+
+  
